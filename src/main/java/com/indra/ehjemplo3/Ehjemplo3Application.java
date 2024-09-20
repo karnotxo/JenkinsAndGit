8e043837ffc041aebfe67ff932b4979249930622
José Manuel Costa Santos <jmcsantos@indra.es>
2024-09-20 12:01:56 +0200

    Initial commit

---
+++ src/main/java/com/indra/ehjemplo3/Ehjemplo3Application.java
@@ -0,0 +1,13 @@
+package com.indra.ehjemplo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ehjemplo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ehjemplo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/indra/ehjemplo3/Ehjemplo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.ehjemplo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ehjemplo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
