9a7b9a99e623f926f33df24499dca7c4f35c36b6
José Manuel Costa Santos <jmcsantos@indra.es>
2024-09-23 10:01:42 +0200

    Initial commit

---
+++ src/main/java/indra/jenkis/examen/ExamenApplication.java
@@ -0,0 +1,13 @@
+package indra.jenkis.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+
+}
